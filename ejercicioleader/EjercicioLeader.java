b8f37107e10d1756730ef13d8e0782cf062971cd
fojomars <fojomars@victoria130.danielcastelao.org>
2018-11-13 08:52:50 +0100

    Segundo commit

--- ejercicioleader/EjercicioLeader.java
+++ ejercicioleader/EjercicioLeader.java
@@ -17,7 +17,7 @@ public class EjercicioLeader {
     public static void main(String[] args) {
         // TODO code application logic here
         int numero;
-        
+        String nombre;
     }
     
 }
